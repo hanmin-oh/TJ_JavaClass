4a4748d4ff712cec721f69109f07a2b6b05372fa
DESKTOP-PODQ609\TJ <hanmin.paul.oh@gmail.com>
2023-04-27 20:43:47 +0900

    0427 study end

--- k20230427/src/com/tjoeun/memoList/DBUtil.java
+++ k20230427/src/com/tjoeun/memoList/DBUtil.java
@@ -25,17 +25,13 @@ public class DBUtil {
 	
 	//데이터베이스 작업에 사용한 객체를 닫는 메소드
 	public static void close(Connection conn) {
-		if (conn != null) { try { conn.close(); } catch (SQLException e) { e.printStackTrace(); } } 
-	}
+		if (conn != null) { try { conn.close(); } catch (SQLException e) { e.printStackTrace(); } } }
 	public static void close(Statement stmt) {
-		if (stmt != null) { try { stmt.close(); } catch (SQLException e) { e.printStackTrace(); } } 
-	} 
+		if (stmt != null) { try { stmt.close(); } catch (SQLException e) { e.printStackTrace(); } } } 
 	public static void close(PreparedStatement pstmt) {
-		if (pstmt != null) { try { pstmt.close(); } catch (SQLException e) { e.printStackTrace(); } } 
-	}	
+		if (pstmt != null) { try { pstmt.close(); } catch (SQLException e) { e.printStackTrace(); } } }	
 	public static void close(ResultSet rs) {
-		if (rs != null) { try { rs.close(); } catch (SQLException e) { e.printStackTrace(); } } 
-	}
+		if (rs != null) { try { rs.close(); } catch (SQLException e) { e.printStackTrace(); } } }
 	
 	
 	
